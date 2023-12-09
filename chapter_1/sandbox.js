016619722ea446cd8d549cbe07e978834583a6bc
Daniel Culpepper <culpepperd2490@gmail.com>
2023-12-08 23:22:31 -0500

    finished 11/188

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,19 +1,36 @@
-// common string methods
+let radius = 10;
+const pi = 3.14;
+console.log(radius, pi);
 
-let email = 'mario@thenetninja.co.uk';
+// math operators: +, -, *, /, **, %
 
-// let result = email.lastIndexOf('n');
+// console.log(10/2);
+// let result = radius % 3;
+// let result = pi * radius**2;
 
-// slice method cuts a section from the string
-// first argument is the starting index,
-// second argument is the ending index.
-// let result = email.slice(1, 12);
+// order of operation: B I D M A S
+// Brackets, Indeces, Division, Multiplication, Addition, Subtraction
+// let result = 5 * (10 - 3)**2;
 
-// substr method creates a substring of characters
-// let result = email.substr(4, 10);
+// console.log(result);
 
-// let result = email.replace('e', 'a');
+let likes = 10;
 
-let result = email.replace('n', 'w');
+// likes = likes + 1;
+//likes++;
+// likes--;
+// likes+=10;
+// likes -= 5;
+// likes *= 2;
+// likes /= 2;
 
+// console.log(likes);
+
+// NaN - not a number
+
+// console.log(5 / 'hello');
+// console.log(5 * 'hello');
+
+// Concatenation
+let result = 'the blog has ' + likes + ' likes';
 console.log(result);
