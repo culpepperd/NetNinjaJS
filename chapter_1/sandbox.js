922b52e4275c07320e9688ab0a13c04b3687b322
Daniel Culpepper <culpepperd2490@gmail.com>
2023-12-11 08:53:22 -0500

    finished 13/188

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,21 +1,22 @@
-// template strings
-const title = 'Best Reads of 2019';
-const author = 'Mario';
-const likes = '30';
+let ninjas = ['Daniel', 'Jack', 'Leyla'];
+let folks = ['Tyrone', 'Renee'];
 
-// concatenation way
-// let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes.';
-// console.log(result);
+// ninjas[2] = 'Renee';
+// console.log(ninjas[2]);
 
-// template string way
-// let result = `There's a blog called "${title}" by ${author} and it has ${likes} likes.`;
-// console.log(result);
+// let ages = [20, 25, 30, 35];
+// console.log(ages[2]);
 
-// creating html templates
-let html = `
-    <h2>${title}</h2>
-    <p>By ${author}</p>
-    <span>This blog has ${likes} likes</span>
-`;
+// let random = ['Daniel', 'Tyrone', 6.5, 11];
+// console.log(random);
 
-console.log(html);
+// console.log(ninjas.length);
+
+// let result = ninjas.join(', ');
+// let result = ninjas.indexOf('Jack');
+// let result = ninjas.concat(folks); // concatenates two arrays
+ninjas.push('Shane');
+ninjas.pop();
+
+
+console.log(ninjas);
