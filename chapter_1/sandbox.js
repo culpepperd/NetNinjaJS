1af7a4137b01c4399e3d188988d09972de74b896
Daniel Culpepper <culpepperd2490@gmail.com>
2023-12-19 15:45:56 -0500

    started 19/188

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,14 +1,7 @@
-// type conversion
+// for loops
 
-let score = '100';
+for(let i = 0; i < 5; i++){
+    console.log('in loop: ', i+1);
+}
 
-// score = Number(score);
-// console.log(score + 1);
-// console.log(typeof score);
-
-// let result = Number('hello');
-// let result = String(50);
-// let result = Boolean(100);
-let result = Boolean('0');
-
-console.log(result, typeof result);
+console.log('loop finished');
