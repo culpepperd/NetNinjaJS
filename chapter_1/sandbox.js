2ffa6b6fc0cfe120498f387e02a7486f97984a03
Daniel Culpepper <culpepperd2490@gmail.com>
2023-12-18 23:52:39 -0500

    started 16/188

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,30 +1,7 @@
-// booleans and comparisons
-console.log(true, false, "true", "false");
+let age = 25;
 
-// methods can return booleans
-let email = 'luigi@thenetninja.co.uk';
-let names = ['mario', 'luigi', 'toad'];
+// loose comparison (different types can still be equal)
 
-// let result = email.includes('!');
-// let result = names.includes('luigi');
+console.log(age == 25);
 
-// console.log(result);
-
-// comparison operators
-let age = 25; 
-
-// console.log(age == 25);
-// console.log(age == 30);
-// console.log(age != 30);
-// console.log(age > 20);
-// console.log(age < 20);
-// console.log(age <= 25);
-// console.log(age >= 25);
-
-let name = 'shaun';
-
-console.log(name == 'shaun');
-console.log(name == 'Shaun');
-console.log(name > 'crystal');
-console.log(name > 'Shaun');
-console.log(name > 'Crystal');
+// strict comparison (different types cannot be equal)
