634f7d9646f97e6e7efcb13bbd45776e23ef25d3
Daniel Culpepper <culpepperd2490@gmail.com>
2023-12-20 15:16:05 -0500

    completed 20/188

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,15 +1,13 @@
-// for loops
+// while loops
 
-// for(let i = 0; i < 5; i++){
-//     console.log('in loop: ', i+1);
+let i = 0;
+const names = ['Daniel', 'Jack', 'Renee'];
+// while(i < 5){
+//     console.log('in loop: ', i);
+//     i++;
 // }
 
-// console.log('loop finished');
-
-const names = ['shaun', 'mario', 'luigi'];
-
-for(let i = 0; i < names.length; i++){
-    // console.log(names[i]);
-    let html = `<div>${names[i]}</div>`;
-    console.log(html);
+while(i < names.length){
+    console.log(names[i]);
+    i++;
 }
