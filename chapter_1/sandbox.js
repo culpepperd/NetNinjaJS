082f330f6fb81c560a3d5e9df86d1044acd49b45
Daniel Culpepper <culpepperd2490@gmail.com>
2023-12-21 13:42:12 -0500

    completed 23/188

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,19 +1,11 @@
-// if statements
+// else if statements
 
-// const age = 20;
+const password = 'p@ss';
 
-// if(age > 19){
-//     console.log('You are over 19 years old.');
-// }
-
-// const ninjas = ['Daniel', 'Ryu', 'Chun-li', 'Captain Baldassare'];
-
-// if(ninjas.length > 3){
-//     console.log("Wow, that's more than 3 ninjas!");
-// }
-
-const password = 'p@ssword';
-
-if(password.length < 8){
-    console.log("The password you entered is too short");
+if(password.length >= 12){
+    console.log("This password is strong like bull!");
+} else if(password.length >= 8){
+    console.log("The password you entered is long enough");
+} else {
+    console.log("Your password is too short.");
 }
