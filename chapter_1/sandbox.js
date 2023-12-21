836fdec25c13168dabf6d48b2bac547b0d36cad0
Daniel Culpepper <culpepperd2490@gmail.com>
2023-12-21 13:53:11 -0500

    completed 24/188

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,11 +1,11 @@
-// else if statements
+// logical operators - OR || and AND &&
 
-const password = 'p@ss';
+const password = 'p@ssw';
 
-if(password.length >= 12){
+if(password.length >= 12 && password.includes('@') || password.includes('$')){
     console.log("This password is strong like bull!");
-} else if(password.length >= 8){
-    console.log("The password you entered is long enough");
+} else if(password.length >= 8 || password.includes('@') && password.length > 4){
+    console.log("The password you entered a little strong...");
 } else {
-    console.log("Your password is too short.");
+    console.log("Your password is too weak.");
 }
