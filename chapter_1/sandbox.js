ee7799f445f8cc36c52e9d98096305b5d7bf5f31
Daniel Culpepper <culpepperd2490@gmail.com>
2023-12-27 17:10:00 -0500

    completed 30/188

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,15 +1,12 @@
-// variables & block scope
-const age = 33;
-
-if(true){
-    const age = 40;
-    const name = "Daniel";
-    console.log("Inside 1st code block: ", name, age);
-
-    if(true){
-        const age = 50;
-        console.log("Inside 2nd code block: ", name, age);
-    }
+// function declaration
+function greet(){
+    console.log("Hello there!");
 }
 
-console.log("Outside code block: ", name, age);
+// function expression
+const speak = function(){
+    console.log("Good day!");
+}
+
+speak();
+// greet();
