779afe9f88f10d84cfa97790f9316a2d3591b017
Daniel Culpepper <culpepperd2490@gmail.com>
2023-12-28 16:01:50 -0500

    completed 33/188

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,12 +1,26 @@
-// regular function
-// const calcArea = function(radius){
-//     return (3.14 * radius**2);
+// practice arrow functions
+
+// const greet = function(){
+//     return "Hello, world!";
 // };
 
-// area function
-// if only ONE parameter, do not need ()
-// if only ONE return statement, format like below
-const calcArea = radius => 3.14 * radius**2;
+// const greet = () => "Hello, world!";
+// console.log(greet());
 
-const area = calcArea(4);
-console.log("The area is:", area);
+// const bill = function (products, tax){
+//     let total = 0;
+//     for(let i=0; i < products.length; i++){
+//         total += products[i] + products[i] * tax;
+//     }
+//     return total;
+// }
+
+const bill = (products, tax) => {
+    let total = 0;
+    for(let i=0; i < products.length; i++){
+        total += products[i] + products[i] * tax;
+    }
+    return total;
+}
+
+console.log(bill([10, 15, 30], 0.2));
