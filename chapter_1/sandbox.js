33d2f2889b95a9acbe31572c56199efc4572a8de
Daniel Culpepper <culpepperd2490@gmail.com>
2024-01-03 14:59:33 -0500

    completed 38/188

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,3 +1,23 @@
-// intro video to Objects
-// Objects have properties and methods
-// Objects have a state and a behavior
+// object literals
+
+let user = {
+    name: 'Daniel',
+    age: 33,
+    email: 'ManMythLegend@gmail.com',
+    location: 'Kentucky', 
+    blogs: ['Star Wars', 'Game of Thrones', 'Marvel']
+};
+
+console.log(user);
+
+// Can access an object's properties either by
+// dot notation or square bracket notation
+user.age = 34;
+console.log(user.age);
+
+console.log(user['location']);
+user['location'] = 'LA';
+console.log(user['location']);
+
+
+
